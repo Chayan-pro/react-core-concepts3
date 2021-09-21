475b801debb8606489a0e489a45494d188823640
Chayan-pro <cschayan4@gmail.com>
2021-09-21 18:55:41 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
+      <LoadCommnet></LoadCommnet>
+    </div>
+  );
+}
+function LoadCommnet (){
+  const [commnets, setCommnet ]= useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res=> res.json())
+    .then(data=> setCommnet(data))
+  },[])
+  return(
+    <div>
+      <h3>Load comment</h3>
+      {
+        commnets.map(commnet => <Commnet title={commnet.title} body={commnet.body}></Commnet>)
+      }
+    </div>
+  )
+}
+function Commnet (props){
+  return(
+    <div style={{background:'lightpink', margin: '20px', borderRadius:'10px', padding:'15px'}}>
+      <h4>{props.title}</h4>
+      <p>{props.body}</p>
+    </div>
+  )
+}
+function Counter(){
+  const [count, setCount] = useState(0);
+  const handIncrease =()=>setCount(count+1);
+  const handDecrease =()=>setCount(count-1);
+  return(
+    <div>
+      <h2>Count:{count} </h2>
+      <button onClick={handIncrease}>Increase</button>
+      <button onClick={handDecrease}>Decrease</button>
     </div>
   );
 }
